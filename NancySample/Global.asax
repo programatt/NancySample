4d5076a8334ddec357db49e7b6927e450d325c19
Matt <mathew.phillips@intlfcstone.com>
2011-06-14 18:58:57 -0500

    initial commit

---
+++ NancySample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NancySample.Global" Language="C#" %>
---
+++ NancySample/Modules/MainModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace NancySample.Modules
+{
+    public class MainModule: NancyModule
+    {
+        public MainModule()
+        {
+            Func<string, string> DoStuff = (name) =>
+                                               {
+                                                   return "Hello " + name;
+                                               };
+
+        Get["/"] = parameters =>
+                           {
+                               return "Hello World!";
+                           };
+            Get["/DoStuff/{name}"] = (name) =>
+                                         {
+                                             return DoStuff(name);
+                                         };
+        }
+
+    }
+}
